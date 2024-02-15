79ffa0905ff2569ff9ad21f3e0289e82a2119414
hayyantahirr <hayyantahir7@gmail.com>
2024-02-15 16:20:53 +0500

    questions added to app.js and they are linked

---
+++ Assignment 2/app.js
@@ -0,0 +1,30 @@
+// Q1 Declare a variable called username
+
+// Q2 Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+
+// Q3 Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+// Q4 Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes
+
+// Q5 Write a script to display the following alert using one JS
+// variable:
+
+// Q6 Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+// Q7 Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box
+
+// Q8 Write a script to display this in browser through JS
+
+// Q9;Store following string in a variable and show in alert and
+// browser through JS
+// “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
---
+++ Assignment 3/app.js
@@ -0,0 +1,19 @@
+// Q1 Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+// ;
+// Q2 Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”;
+// Q3 Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser;
+// Q4 A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”;
+
